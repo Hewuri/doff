61d16bfc3731cb002c8e5beb316dc6516e92bb0e
Stefan Richter <Hewuri@googlemail.com>
2014-07-14 22:38:27 +0200

    added script for analyzing gewiss

--- scripts/analyzeGewiss.py
+++ scripts/analyzeGewiss.py
@@ -5,7 +5,6 @@ import os, codecs
 from string import punctuation
 import datetime
 
-
 def cleanText(text):
     for c in punctuation:
         text = text.replace(c, " ")
@@ -18,57 +17,20 @@ def createSuggestion(l):
         return "[" + l[0] + "=" + l[1] + ", " + l[2] + "=" + l[3] + "]"
     return "[no suggestion available]"
 
-sentence = "dies ist ein test jetzt werde ich konkret und sage meine kritik"
-for word in sentence.split():
-    if isFalseFriend(word):
-        print(word + " " +  createSuggestion(getSuggestion(word)))
-
-
-'''w = list()
-def analyze(dir_input, name):
-    print(str(datetime.datetime.now().time()) +  " start analyze " + name )
-    nodeSoap = etree.Element(name.replace(" ","_"))
-    files = ['testdata.xml'] if name == "Testdata" else os.listdir(dir_input)
+def analyze(dir_input):
+    print(str(datetime.datetime.now().time()) +  " start analyze " + dir_input )
+    files = os.listdir(dir_input)
     files.sort()
 
     for file in files:
         xml_text = codecs.open(dir_input + file, "r", "utf-8")
         xml = etree.parse(xml_text)
-        text = ""
-        for element in xml.iter("p"):
-            if element.text != None:
-                text += " "  + cleanText(element.text)
-        init_postagger(text)
-        nodeEpisode = etree.Element("episode_" +file.replace(".xml", ""))
-        nodeEpisode.attrib["year"] = xml.getroot().attrib["year"]
-        for element in xml.iter("p"):
-            if element.text == None or element.text.strip() == "Dahoam is Dahoam.":
-                continue
+        for element in xml.iter("event"):
+            if element.text == None:
+               pass
             sentence = cleanText(element.text)
-            nodeText = ""
             for word in sentence.split():
-                if isAnglicism(word, sentence):
-                    nodeText += "<a>" + word +"</a> "
-                    w.append(word)
-                    print(word + " -> " + sentence + " -> " + file)
-                else:
-                    nodeText += word + " "
-            if "</a>" in nodeText:
-                nodeElement = element
-                nodeElement.text = nodeText
-                nodeEpisode.append(nodeElement)
-        nodeSoap.append(nodeEpisode)
-    print(str(datetime.datetime.now().time()) +  " finish analyze " + name)
-    return nodeSoap
+                if isFalseFriend(word):
+                    print(word + " " + createSuggestion(getSuggestion(word)))
 
-
-resultXml = etree.Element('root')
-#resultXml.append(analyze('../scripts/', 'Testdata'))
-resultXml.append(analyze('../subtitles/Lindenstrasse/normalized/', 'Lindenstrasse'))
-resultXml.append(analyze('../subtitles/Dahoam is Dahoam/normalized/', 'Dahoam is Dahoam'))
-
-file = open("results/result.xml","w")
-file.write(etree.tostring(resultXml, pretty_print=True))
-file.close()
-
-'''
+analyze('../gewiss/DEU_L1_EV/')
